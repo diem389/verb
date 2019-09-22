9e02a7a575f430b767c1f87219d7a3daaf6b0ee7
Jaeyoon Jeong <diem389@gmail.com>
2019-09-22 10:02:18 +0900

    rebuild

--- src/verb/core/Binomial.hx
+++ src/verb/core/Binomial.hx
@@ -3,84 +3,79 @@ package verb.core;
 import haxe.ds.IntMap;
 
 class Binomial {
+	static var memo = new IntMap<IntMap<Float>>();
 
-    static var memo = new IntMap<IntMap<Float>>();
+	public static function get(n:Int, k:Int):Float {
+		if (k == 0.0) {
+			return 1.0;
+		}
 
-    public static function get(n : Int, k : Int) : Float {
-        if (k == 0.0) {
-            return 1.0;
-        }
+		if (n == 0 || k > n) {
+			return 0.0;
+		}
 
-        if (n == 0 || k > n) {
-            return 0.0;
-        }
+		if (k > n - k) {
+			k = n - k;
+		}
 
-        if (k > n - k) {
-            k = n - k;
-        }
+		if (memo_exists(n, k)) {
+			return get_memo(n, k);
+		}
 
-        if ( memo_exists(n,k) ) {
-            return get_memo(n,k);
-        }
+		var r:Float = 1, n_o = n;
 
-        var r : Float = 1,
-        n_o = n;
+		for (d in 1...k + 1) {
+			if (memo_exists(n_o, d)) {
+				n--;
+				r = get_memo(n_o, d);
+				continue;
+			}
 
-        for (d in 1...k+1){
+			r *= n--;
+			r /= d;
 
-            if ( memo_exists(n_o, d) ) {
-                n--;
-                r = get_memo(n_o, d);
-                continue;
-            }
+			memoize(n_o, d, r);
+		}
 
-            r *= n--;
-            r /= d;
+		return r;
+	}
 
-            memoize(n_o, d, r);
+	public static function get_no_memo(n:Int, k:Int):Float {
+		if (k == 0) {
+			return 1;
+		}
 
-        }
+		if (n == 0 || k > n) {
+			return 0;
+		}
 
-        return r;
-    }
+		if (k > n - k) {
+			k = n - k;
+		}
 
-    public static function get_no_memo(n : Int, k : Int) : Float {
-        if (k == 0) {
-            return 1;
-        }
+		var r:Float = 1, n_o = n;
 
-        if (n == 0 || k > n) {
-            return 0;
-        }
+		for (d in 1...k + 1) {
+			r *= n--;
+			r /= d;
+		}
 
-        if (k > n - k) {
-            k = n - k;
-        }
+		return r;
+	}
 
-        var r : Float = 1,
-        n_o = n;
+	private static function memo_exists(n:Int, k:Int):Bool {
+		return (memo.exists(n) && memo.get(n).exists(k));
+	}
 
-        for (d in 1...k+1){
-            r *= n--;
-            r /= d;
-        }
+	private static function get_memo(n:Int, k:Int):Float {
+		return memo.get(n).get(k);
+	}
 
-        return r;
-    }
+	private static function memoize(n, k, val) {
+		if (!memo.exists(n)) {
+			memo.set(n, new IntMap<Float>());
+		}
 
-    private static function memo_exists(n : Int, k : Int) : Bool {
-        return ( memo.exists(n) && memo.get(n).exists(k) );
-    }
-
-    private static function get_memo(n : Int, k : Int) : Float {
-        return memo.get(n).get(k);
-    }
-
-    private static function  memoize(n, k, val) {
-        if ( !memo.exists(n) ) {
-            memo.set( n, new IntMap<Float>() );
-        }
-
-        memo.get( n ).set( k, val );
-    }
+		memo.get(n).set(k, val);
+	}
 }
